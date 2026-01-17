90b26ab7cddaf59bd4bb5b05a9302a2ef7ecde09
NaraChou <exloe574@gmail.com>
2026-01-17 20:45:15 +0800

    修改細節

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import * as path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 // 如果這行還是紅字，請確保你已經跑過上面的 npm install 指令
 // @ts-ignore
-import react from '@vitejs/plugin-react';
-// @ts-ignore
 import tailwindcss from '@tailwindcss/vite' // 新增這行
+// @ts-ignore
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/KikiDesign/', // GitHub 佈署關鍵
     plugins: [
-      react(),
-      tailwindcss(), // 新增這行
+      tailwindcss(), // 將 tailwind 放在 react 之前通常更穩定
+      react(),      
     ],
     resolve: {
       alias: {
